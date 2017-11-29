70a0ae3f6efd1e24411e63aa08d6226057a84f90
Stella <sgordonzigel@gmail.com>
2017-11-29 10:34:46 -0500

    workout tracking working

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,6 +2,7 @@ class EntriesController < ApplicationController
 
   def index
     @user = current_user
+
     if params[:workout_id]
       @entries = Workout.find(params[:workout_id])
     else
@@ -16,14 +17,14 @@ class EntriesController < ApplicationController
 
   def create
     @workout = Workout.find(params[:workout_id])
-    @entry = @workout.entries.create(entry_params)
+    @entry = @workout.entries.create(entry_params.merge(user: current_user))
 
-    redirect_to workout_entries_path(@workout)
+    redirect_to entries_path
   end
 
   private
   def entry_params
-    params.require(:entry).permit(:duration, :date)
+    params.require(:entry).permit(:duration, :date, :comments, :workout_id, :user_id)
   end
 
 end
